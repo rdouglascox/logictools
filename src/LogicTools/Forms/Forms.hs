27fbba3db0d45d1b2f091cb4ba9c054702ce4928
Ryan Cox <ryan.cox@sydney.edu.au>
2023-06-29 12:17:09 +1000

    adding to logical forms

--- src/LogicTools/Forms/Forms.hs
+++ src/LogicTools/Forms/Forms.hs
@@ -1,6 +1,7 @@
 module LogicTools.Forms.Forms where
 
 import LogicTools.Data.PLProp
+import Data.List.Unique
 
 -- | Data type for Logical Forms  
 data Form =
@@ -11,6 +12,19 @@ data Form =
    | ConditionalForm Form Form 
    | BiconditionalForm Form Form 
    deriving (Show, Eq, Ord) 
+
+-- | Enhanced data type for propositions including indexing
+-- where propositions are identical to other subpropositions
+-- of the proposition 
+data IndexedProp = 
+     IndexedBasic (Maybe Int) RawBasic 
+   | IndexedTop (Maybe Int)   
+   | IndexedBottom (Maybe Int) 
+   | IndexedNegation (Maybe Int) Prop 
+   | IndexedConjunction (Maybe Int) Prop Prop
+   | IndexedDisjunction (Maybe Int) Prop Prop
+   | IndexedConditional (Maybe Int) Prop Prop
+   | IndexedBiconditional (Maybe Int) Prop Prop
 
 -- | Get all the subformulas of a formula
 subformulas :: Prop -> [Prop] 
@@ -23,5 +37,10 @@ subformulas (Disjunction x y) = Disjunction x y : (subformulas x ++ subformulas 
 subformulas (Conditional x y) = Conditional x y : (subformulas x ++ subformulas y)
 subformulas (Biconditional x y) = Biconditional x y : (subformulas x ++ subformulas y)
 
+-- | We want to get a list of duplicate props from the
+-- subformulas and then index them
+
+duplicates :: [Prop] -> [[Prop]] 
+duplicates xs = filter (\x -> length x > 1) (group xs)  
 
 
